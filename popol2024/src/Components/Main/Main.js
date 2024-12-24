efa4e1478fc9cac3ecb3fd7b54827ba136d32c43
kimJAVAN <ookim7717@gmail.com>
2024-12-24 17:33:46 +0900

    Initial commit

---
+++ popol2024/src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css'
+
+function Main() {
+  return (
+    <div>
+      <h1>메인입니다</h1>
+    </div>
+  );
+}
+
+export default Main;
---
+++ popol2024/src/Components/Popol/Popol.js
@@ -0,0 +1,44 @@
+import './Popol.css'
+
+function Popol() {
+  return (
+    <div>
+      <div className='main-popol-wrap'>
+
+        <div className='popol-btn-wrap'>
+          <div className='popol-btn'>
+            <p>React</p>
+          </div>
+          <div className='popol-btn'>
+            <p>JavaScript</p>
+          </div>
+          <div className='popol-btn'>
+            <p>CSS</p>
+          </div>
+          <div className='popol-btn'>
+            <p>HTML</p>
+          </div>
+          <div className='popol-btn'>
+            <p>REST API</p>
+          </div>
+          <div className='popol-btn'>
+            <p>NEXT.js</p>
+          </div>
+          <div className='popol-btn'>
+            <p>Node.js</p>
+          </div>
+        </div>
+
+        <div className='popol-unit-wrap'>
+          <div className='popol-unit-div'>
+            <div className=''>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Popol;
